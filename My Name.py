4bf3688b1f9db996d64309a6c35a0e74e427c252
mfanelo007 <154869090+mfanelo007@users.noreply.github.com>
2024-01-19 13:07:33 +0200

    Add files via upload

---
+++ My Name.py
@@ -0,0 +1,6 @@
+coffee = "# How I make a cup of coffee"
+ingredients = "\n ## Ingredients"
+list1 = "\n 1. Nescafe coffee \n 2. Boiling water \n 3. Brown sugar \n"
+way = "## How to make \n 1. Put 1 teaspoon of Nescafe coffe and 3 teaspoons of brown sugar in to a mug \n 2. Boil water in a kettle \n 3. Pour the boiling water in to the mug and stir"
+done = "\n Bon appetit motherfuckers!"
+print(coffee, ingredients, list1, way, done)
